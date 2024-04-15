d29847b1cfdaf91abdb6830c13e1210d882af8b5
Eric Tashji <99293073+etashji@users.noreply.github.com>
2024-04-15 16:09:07 -0400

    Resolved Conflicts

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 import userRoutes from './users.js';
-// import journalRoutes from './journals.js';
-// import sectionRoutes from './sections.js';
-// import postRoutes from './posts.js';
-// import commentRoutes from './comments.js';
+import journalRoutes from './journals.js';
+import sectionRoutes from './sections.js';
+import postRoutes from './posts.js';
+import commentRoutes from './comments.js';
 
 const constructorMethod = (app) => {
     app.use('/users', userRoutes);
-    // app.use('/journals', journalRoutes);
-    // app.use('/sections', sectionRoutes);
-    // app.use('/posts', postRoutes);
-    // app.use('/comments', commentRoutes);
+    app.use('/journals', journalRoutes);
+    app.use('/sections', sectionRoutes);
+    app.use('/posts', postRoutes);
+    app.use('/comments', commentRoutes);
 
     app.use("*", (req, res) => {
         res.status(404).json({error: 'Route Not Found'});
