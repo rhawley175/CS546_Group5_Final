8184235733e591616dc96092dd2bf7867012dfcb
Eddie Kuang <67530038+ekuang7@users.noreply.github.com>
2024-04-16 12:06:10 -0700

    posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -7,24 +7,25 @@ const router = Router();
 router
 .route('/newPost')
 .get(async(req,res) => {
-    res.render('newPost', {title: 'New Post'});   //change handlebar to entry name.
+    res.render('posts/newPost', {title: 'New Post'});   //change handlebar to entry name.
 })
 .post(async(req,res) => {
     const data=req.body;
     
     
     
+    
     try{
-        const entry = await addPost(data.titleInput, data.entryText, data.pub);
-        
+        const entry = await addPost(data.sectionIdInput, data.titleInput, data.entryText, data.pub, data.usernameInput);
+
         if(entry){
-            return res.render('newPost', {title: 'New Post'});
+            return res.render('posts/newPost', {title: 'New Post', success: true, postId: entry});
         }
         else{
-            res.status(500).render('newPost', {hasError: true, error: 'Internal Server Error.', title: 'New Post'});
+            res.status(500).render('posts/newPost', {hasError: true, error: 'Internal Server Error.', title: 'New Post'});
         }
     }
     catch(e){
-        res.status(400).render('newPost', {hasError: true, error: e, title: 'New Post'});
+        res.status(400).render('posts/newPost', {hasError: true, error: e, title: 'New Post'});
     }
 });
