2ae5dd219dec4f34fbabacb707c203168cb57c4a
Eddie Kuang <67530038+ekuang7@users.noreply.github.com>
2024-04-16 01:12:54 -0700

    posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,30 @@
 import {Router} from 'express';
+import {addPost, getPost, deletePost} from '../data/posts.js';
+
 const router = Router();
 
-export default router;
+
+router
+.route('/newPost')
+.get(async(req,res) => {
+    res.render('newPost', {title: 'New Post'});   //change handlebar to entry name.
+})
+.post(async(req,res) => {
+    const data=req.body;
+    
+    
+    
+    try{
+        const entry = await addPost(data.titleInput, data.entryText, data.pub);
+        
+        if(entry){
+            return res.render('newPost', {title: 'New Post'});
+        }
+        else{
+            res.status(500).render('newPost', {hasError: true, error: 'Internal Server Error.', title: 'New Post'});
+        }
+    }
+    catch(e){
+        res.status(400).render('newPost', {hasError: true, error: e, title: 'New Post'});
+    }
+});
