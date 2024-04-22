ecaf75283a0e11496c4eb10b3f9049968395289e
Jince Shi <120279910+andy8th@users.noreply.github.com>
2024-04-22 17:00:24 -0400

    Update journals.js

--- data/journals.js
+++ data/journals.js
@@ -2,21 +2,22 @@ import { journals } from '../config/mongoCollections.js';
 import { ObjectId } from 'mongodb';
 import * as helpers from '../helpers.js';
 
-export const createJournal = async (userId, title, sectionTitle, sectionContent) => {
+export const createJournal = async (userId, username, title, sectionTitle, sectionContent) => {
   try {
-    userId = helpers.checkString(userId, "User ID");
+    username = helpers.checkString(username, "Username");
     title = helpers.checkString(title, "Title");
     sectionTitle = helpers.checkString(sectionTitle, "Section Title");
     sectionContent = helpers.checkString(sectionContent, "Section Content");
 
     const journalCollection = await journals();
     const newJournal = {
-      userId: new ObjectId(userId),
-      author: [userId],
+      user_id: [userId],
+      author: [username],
       title: title,
       sections: [
         {
           title: sectionTitle,
+          content: sectionContent,
           posts: [],
         },
       ],
@@ -26,8 +27,7 @@ export const createJournal = async (userId, title, sectionTitle, sectionContent)
     if (!insertInfo.acknowledged || !insertInfo.insertedId)
       throw 'Could not add journal';
 
-    const newId = insertInfo.insertedId.toString();
-    return await getJournalById(newId);
+    return await getJournalById(insertInfo.insertedId);
   } catch (error) {
     console.error('Error in createJournal:', error);
     throw error;
@@ -35,7 +35,6 @@ export const createJournal = async (userId, title, sectionTitle, sectionContent)
 };
 
 export const getJournalById = async (journalId) => {
-  journalId = helpers.checkString(journalId, "Journal ID");
   const journalCollection = await journals();
   const journal = await journalCollection.findOne({ _id: new ObjectId(journalId) });
   if (!journal) throw 'Journal not found';
@@ -45,19 +44,17 @@ export const getJournalById = async (journalId) => {
 export const getJournalsByUser = async (userId) => {
   userId = helpers.checkString(userId, "User ID");
   const journalCollection = await journals();
-  return await journalCollection.find({ userId: new ObjectId(userId) }).toArray();
+  return await journalCollection.find({ user_id: userId }).toArray();
 };
 
 export const updateJournal = async (journalId, updatedJournal) => {
   journalId = helpers.checkString(journalId, "Journal ID");
   const journalCollection = await journals();
-
   const updatedJournalData = {};
 
   if (updatedJournal.title) {
     updatedJournalData.title = helpers.checkString(updatedJournal.title, "Title");
   }
-
   if (updatedJournal.sections) {
     updatedJournalData.sections = helpers.checkArray(updatedJournal.sections, "Sections");
   }
@@ -66,7 +63,6 @@ export const updateJournal = async (journalId, updatedJournal) => {
     { _id: new ObjectId(journalId) },
     { $set: updatedJournalData }
   );
-
   if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
     throw 'Update failed';
 
@@ -77,7 +73,6 @@ export const deleteJournal = async (journalId) => {
   journalId = helpers.checkString(journalId, "Journal ID");
   const journalCollection = await journals();
   const deleteInfo = await journalCollection.deleteOne({ _id: new ObjectId(journalId) });
-
   if (deleteInfo.deletedCount === 0) {
     throw `Could not delete journal with id ${journalId}`;
   }
