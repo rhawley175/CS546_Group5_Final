483ca8b755914802c93a1c73f5a6750062e3ede7
Jince Shi <120279910+andy8th@users.noreply.github.com>
2024-04-23 07:44:07 -0400

    Update journals.js

--- routes/journals.js
+++ routes/journals.js
@@ -30,11 +30,11 @@ router.get('/create', async (req, res) => {
 router.post('/create', async (req, res) => {
   try {
     if (!req.session.user) {
-      return res.redirect('/users/login'); // Redirect to the login page if user is not authenticated
+      return res.redirect('/users/login'); 
     }
-    const userId = req.session.user._id; // Extract the userId (ObjectId) from the authenticated user session
-    const username = req.session.user.username; // Extract the username from the authenticated user session
-    console.log('userId:', userId); // Log the userId
+    const userId = req.session.user._id; 
+    const username = req.session.user.username; 
+    console.log('userId:', userId);
     const { title, sectionTitle, sectionContent } = req.body;
     const newJournal = await createJournal(userId, username, title, sectionTitle, sectionContent);
     res.redirect(`/journals/${newJournal._id}`);
@@ -54,14 +54,34 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const journalId = req.params.id;
+    const journal = await getJournalById(journalId);
+    res.render('journals/editJournal', { journal });
+  } catch (error) {
+    res.status(404).json({ error: 'Journal not found' });
+  }
+});
+
 router.put('/:id', async (req, res) => {
   try {
     const journalId = req.params.id;
     const updatedJournal = req.body;
     const journal = await updateJournal(journalId, updatedJournal);
-    res.json(journal);
+    res.redirect(`/journals/${journalId}`);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/:id/delete', async (req, res) => {
+  try {
+    const journalId = req.params.id;
+    const journal = await getJournalById(journalId);
+    res.render('journals/deleteJournal', { journal });
+  } catch (error) {
+    res.status(404).json({ error: 'Journal not found' });
   }
 });
 
@@ -69,7 +89,7 @@ router.delete('/:id', async (req, res) => {
   try {
     const journalId = req.params.id;
     await deleteJournal(journalId);
-    res.sendStatus(204);
+    res.redirect('/journals');
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
